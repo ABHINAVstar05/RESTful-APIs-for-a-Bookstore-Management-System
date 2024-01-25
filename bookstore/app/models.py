b715ae8df1e841bfec06821b155d638357c3ab06
Abhinav <kumar.abhinav3125@gmail.com>
2024-01-26 00:38:08 +0530

    Created Django models to design a database schema to store book details and user details.

--- bookstore/app/models.py
+++ bookstore/app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+
+"""  
+Model to define structure of table for storing book details in the DB.
+"""
+class bookDetails(models.Model) :
+    ISBN = models.IntegerField(primary_key = True)
+    title = models.CharField(max_length = 100)
+    author = models.CharField(max_length = 100)
+    price = models.IntegerField()
+    quantity = models.IntegerField()
+    
+    def __str__(self):
+        return f"{self.title} -by ({self.author})"
+    
+    
+"""  
+Model to define structure of table for storing user details in the DB.
+"""
+class users(models.Model) :
+
+    ROLE_CHOICES = [
+        ('Owner', 'Owner'),
+        ('Customers', 'Customers'),
+    ]
+
+    name = models.CharField(max_length = 100)
+    role = models.CharField(max_length = 20, choices = ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.name} ({self.role})"
